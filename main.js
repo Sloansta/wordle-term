388f9215b4eb0f0fdf76eef13c25fcc103bc9796
Sloan <Sloan.boyce@gmail.com>
2022-02-05 22:48:51 -0500

    checkWord function now works, highlighting is fully functional. All there is to do now is add user input and win/loss functionality

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import randomWords from 'random-words';
 import chalk from 'chalk';
 const word = findWordleWord();
+let guessCount = 0;
+const maxGuessCount = 6;
 
 // using recursian to ensure that the user will get a 5 letter word without calling for too big of a word count
 function findWordleWord() {
@@ -13,4 +15,21 @@ function findWordleWord() {
     findWordleWord();
 }
 
-console.log(chalk.white.bgYellow(word));
+// this will check to see if the characters match in the word
+// What we will want to do is take the generated word, and then match it against the guess character by character 
+function checkLetter(wrd, guess) {
+    const wordArr = guess.split('');
+    for(let i = 0; i < wordArr.length; i++) {
+        if(wordArr[i] == wrd[i]) 
+            wordArr[i] = chalk.white.bgGreen(wordArr[i].toUpperCase());
+        else if(wrd.includes(wordArr[i])) 
+            wordArr[i] = chalk.white.bgYellow(wordArr[i].toUpperCase());
+        else 
+            wordArr[i] = chalk.white.bgBlack(wordArr[i].toUpperCase());
+    }
+
+    const cycledWord = wordArr.join('|');
+    return cycledWord;
+}
+
+console.log(checkLetter(word, 'water'));
